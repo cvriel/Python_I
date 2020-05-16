0311c0386a90f5b5741b63d3a3738f5ba651f40b
Chris van Riel <c.van.riel@amsterdam.nl>
2020-05-16 14:53:23 +0200

    added viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
+from rest_framework import viewsets
 
 
 class HelloApiView(APIView):
@@ -43,3 +44,47 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """handle the delete of an object"""
         return Response({"methode": "DELETE"})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """ Return a hello message"""
+
+        a_viewset = [
+            "Uses action (list, create, retrieve, update, partial_update)",
+            "Automatically maps to URLs using Routers",
+            "Provides more functionality with less code",
+        ]
+
+        return Response({"message": "Hello!", "a_viewset": a_viewset})
+
+    def create(self, request):
+        """ create a new hello message """
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f"Hello {name}"
+            return Response({"message": message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """ Handle getting an object by its ID """
+        return Response({"http_messsage": "GET"})
+
+    def update(self, request, pk=None):
+        """ Handle updating an object """
+        return Response({"http_message": "PUT"})
+
+    def partial_update(self, request, pk=None):
+        """ Handle a partial update """
+        return Response({"http_message": "PATCH"})
+
+    def destroy(self, request, pk=None):
+        """ Handle a delete """
+        return Response({"http_message": "DELETE"})
